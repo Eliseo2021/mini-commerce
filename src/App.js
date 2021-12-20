161910f28feda74b89e558fac61fd44d9cb0a041
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2021-12-20 17:30:56 -0300

    3- Agregando componentes bootstrap - nav y home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-import { HeaderApp } from "./components/HeaderApp";
-import { ListProducts } from "./components/ListProducts";
+import  HeaderApp  from "./components/HeaderApp";
+import { BrowserRouter } from 'react-router-dom';
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <Container>
       <div className="App">
-        <HeaderApp />
-        <ListProducts />
+        <BrowserRouter>
+          <HeaderApp />
+        </BrowserRouter>
+        <Home />
       </div>
     </Container>
   );
--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,17 +1,67 @@
-import React from "react";
-import { Row } from "react-bootstrap";
-import json from "../examples/products.json";
-import { Product } from "./Product";
-
+import React, { useState, useEffect } from "react";
 
 export const ListProducts = () => {
-  const products = json.products;
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+
+  let componentMounted = true;
+
+  useEffect(() => {
+    const getProducts = async () => {
+
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        console.log(filter);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+
+    getProducts();
+  }, []);
+
+
+  const ShowProducts = () => {
+    return (
+      <>
+        {filter.map((product, index) => {
+          return (
+            <>
+              <div className="col-md-3 mb-4">
+                <div className="card h-100 text-center p-4" key={index} >
+                  <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                    <p className="card-text lead fw-bold">${product.price}</p>
+                  </div>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </>
+    );
+  };
 
   return (
-    <Row>
-      {products.map((product, index) => (
-        <Product key={index} item={product} />
-      ))}
-    </Row>
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">
+              Listado de Productos
+            </h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {<ShowProducts />}
+        </div>
+      </div>
+    </div>
   );
 };
+
