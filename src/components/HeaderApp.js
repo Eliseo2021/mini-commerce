4e71e98ea0cc66f3d432ef7fa4bf83a4c3ad37db
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2021-12-29 19:40:31 -0300

    carrito-de-compra

--- src/components/HeaderApp.js
+++ src/components/HeaderApp.js
@@ -1,55 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Navbar, Container } from "react-bootstrap";
+import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
 
-export default function HeaderApp() {
-  return (
-      <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
-        <div className="container">
-          <NavLink className="navbar-brand fw-bold fs-4" to="/">
-            LA COLECCION
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="products">
-                  Productos
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  Nosotros
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contacto
-                </NavLink>
-              </li>
-            </ul>
-            <div className="buttons">              
-              <NavLink to="/cart" className="btn btn-outline-dark ms-2">
-                Carrito (0)</NavLink>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+const style = {
+  marginBottom: '30px'
 }
+
+export const HeaderApp = () => (
+  <Navbar bg="dark" variant="dark" style={style}>
+    <Container>
+      <Navbar.Brand href="#home">
+      Mini Ecommerce
+      </Navbar.Brand>
+      <ShoppingCart />
+    </Container>
+  </Navbar>
+);
